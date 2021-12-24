1b6936c684634ae7f23debdb3696940d154c6029
Ni3RaN <wzh47539401@gmail.com>
2021-12-24 11:38:36 +0800

    last

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-mongoose.connect('mongodb://localhost/chatroom');
+mongoose.connect('mongodb://localhost/190110910625');
 const userSchema = new Schema({
     nickname: {
         type: String,
